1729603101fec6fc3e8650e1fe8819e88498f7f1
Alfiya Abdimutalipova <114033078+abdimutalipova-alfiya@users.noreply.github.com>
2024-12-17 20:28:26 +0000

    .

--- pages/Question_Answering_Tool.py
+++ pages/Question_Answering_Tool.py
@@ -187,10 +187,16 @@ with bottom():
 # Button stays in the smaller column
     with cols[1]:
         # if st.button("🎤"):
-        audio = audio_recorder(sample_rate=16000)
+        audio = audio_recorder(
+        sample_rate=16000,
+        text="🎤",
+        recording_color="#e8b62c",
+        neutral_color="#6aa36f",
+        icon_name="mic",
+        icon_size="2x"
+    )
         if audio:
             st.audio(audio, format="audio/wav")
-            st.write(f"Audio data size: {len(audio)} bytes")
             audio_data = np.frombuffer(audio, dtype=np.int16)
             voice_query = voice_input_handler.process_voice_query(audio_data)
             if voice_query:
