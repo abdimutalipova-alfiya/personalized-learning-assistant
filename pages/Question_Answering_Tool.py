ccb236e971154182295938fe6ace720654c2d2d4
Alfiya Abdimutalipova <114033078+abdimutalipova-alfiya@users.noreply.github.com>
2024-12-17 20:37:35 +0000

    .

--- pages/Question_Answering_Tool.py
+++ pages/Question_Answering_Tool.py
@@ -186,13 +186,9 @@ with bottom():
     prompt = None
 # Button stays in the smaller column
     with cols[1]:
-        st.write("Voice Input")
-        audio = audio_recorder(
-        sample_rate=16000,
-        text="🎤",
-        recording_color="#e8b62c",
-        neutral_color="#6aa36f",
-        icon_name="mic"    )
+        # if st.button("🎤"):
+        audio = audio_recorder(sample_rate=16000)
+        
         if audio:
             st.audio(audio, format="audio/wav")
             audio_data = np.frombuffer(audio, dtype=np.int16)
